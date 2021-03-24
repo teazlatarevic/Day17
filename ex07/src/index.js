a7b03c55a4981c581b72fb8e7a8300b3c10a8c47
Tea Zlatarevic <tea.zlatarevic@gmail.com>
2021-03-24 16:15:13 +0100

    Solution for the example 7

---
+++ ex07/src/index.js
@@ -0,0 +1,16 @@
+const range = {
+  max: 100.0,
+  min: -25.0,
+  num1: 17.5,
+  num2: 88.8,
+  average: 45.325,
+};
+// Only change code below this line
+
+const middle = ({ max, min }) => {
+  return (max + min) / 2.0;
+};
+
+// Only change code above this line
+console.log(middle(range));
+module.exports = middle;
