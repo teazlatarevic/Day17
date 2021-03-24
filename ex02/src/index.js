429ea3948a1c14b3a573cb9a4a8db4dd96429930
Tea Zlatarevic <tea.zlatarevic@gmail.com>
2021-03-24 11:59:27 +0100

    Solution for the example2

---
+++ ex02/src/index.js
@@ -0,0 +1,17 @@
+const MY_CAR = {
+    wheels: 4,
+    cylinders: 4,
+    size: 2.2
+};
+
+function main(MyCar) {
+    // Only change code below this line
+
+    const { cylinders, size } = MY_CAR;
+    return { cylinders, size };
+
+    // Only change code above this line
+}
+
+console.log(main(MY_CAR));
+module.exports = main;
